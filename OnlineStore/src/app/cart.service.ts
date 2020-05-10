1a74284e36fc8337d5c4febedd2819ce76fae60b
Sandeep Gopan <sandeepgopan111@gmail.com>
2020-05-10 21:23:51 +0530

    commit

--- OnlineStore/src/app/cart.service.ts
+++ OnlineStore/src/app/cart.service.ts
@@ -7,18 +7,6 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CartService {
   items = [];
-  addToCart(product) {
-    this.items.push(product);
-  }
-
-  getItems() {
-    return this.items;
-  }
-
-  clearCart() {
-    this.items = [];
-    return this.items;
-  }
   constructor(
     private http: HttpClient
   ) {}
